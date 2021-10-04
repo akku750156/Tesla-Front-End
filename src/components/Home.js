96cbc26e07814a40aed921d97c2ae98ada7d62fd
Akash Tiwari <63925981+akku750156@users.noreply.github.com>
2021-10-04 15:27:39 +0530

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+
+function Home() {
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        description="Order Online fro TouchLess Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online fro TouchLess Delivery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order Online fro TouchLess Delivery"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Lowest Cost Solar Panels in America"
+        description="Money-back guarantee"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+      />
+      <Section
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop Now"
+      />
+    </Container>
+  );
+}
+
+export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+`;
